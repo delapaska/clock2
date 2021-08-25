cd69e9984dbb28031178c565fa4528b9b9f9855a
delap <delapaska12@yandex.ru>
2021-08-25 21:21:54 +0300

    add: todo's file

---
+++ app/src/main/java/com/example/clock2/TODO.kt
@@ -0,0 +1,5 @@
+package com.example.clock2
+
+//TODO Share current time via Intent
+//TODO ConstraintLayout, LinearLayout, RelativeLayout, ScrollView, ImageView, RecyclerView (ViewHolder, Adapter, Recycler View Item)
+//TODO styles.xml
